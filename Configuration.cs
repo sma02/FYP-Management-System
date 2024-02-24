947968d96ed226fede0e253222c5dd89a2a4b9a1
sma02 <sma02022@outlook.com>
2024-02-24 20:13:00 +0500

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+namespace FYP_Management_System
+{
+    class Configuration
+    {
+        String ConnectionStr = @"Data Source=(local);Initial Catalog=ProjectA;Integrated Security=True; Encrypt=False";
+        SqlConnection con;
+        private static Configuration _instance;
+        public static Configuration getInstance()
+        {
+            if (_instance == null)
+                _instance = new Configuration();
+            return _instance;
+        }
+        private Configuration()
+        {
+            con = new SqlConnection(ConnectionStr);
+            con.Open();
+        }
+        public SqlConnection getConnection()
+        {
+            return con;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FYP_Management_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnManageStudents_Click(object sender, RoutedEventArgs e)
+        {
+            var conn = Configuration.getInstance().getConnection();
+            SqlCommand command = new SqlCommand("SELECT * FROM Person", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            DG1.ItemsSource = table.DefaultView;
+        }
+    }
+}
